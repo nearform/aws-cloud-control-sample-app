8432bb766be5ab1235d589a99d6084937b420f5b
Sinead Bunyan <sinead@visua.com>
2021-10-08 08:19:58 +0100

    Update terraform backend config to point to resources in the new Nearform AWS account

--- infrastructure/environment/sample-app/terraform_backend.tf
+++ infrastructure/environment/sample-app/terraform_backend.tf
@@ -3,11 +3,11 @@ terraform {
   required_version = "~> 1.0.8"
 
   backend "s3" {
-    bucket         = "nearform-platform-infrastructure"
+    bucket         = "nearform-terraform-infrastructure"
     key            = "terraform-state-sample-app.json"
     region         = "eu-west-1"
     encrypt        = true
-    dynamodb_table = "platform-state-lock"
+    dynamodb_table = "terraform-state-lock"
     profile        = "nearform-platform"
   }
 }
